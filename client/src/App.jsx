f3e68af079ea328d91338413a8b8acc4f36ad76e
KnightMphande <sibusiso.mpande@gmail.com>
2024-09-29 01:45:28 +0200

    header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/header/Header";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <>
+    <Header />
+        <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+    </h1></>
   )
 }
